84a8b676bf5f42462dd51a0d6150e35fdee69ebd
Frederico <layeph@gmail.com>
2020-01-16 15:52:10 -0300

    Day 3
    Completed day 3 (no extras)

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://Layeph:teste@omniweek10layeph-w2aet.gcp.mongodb.
   useCreateIndex: true    
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
